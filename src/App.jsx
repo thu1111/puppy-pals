28db91387e7316fc62967d83d2601ef6ea3d80bb
Thu Yein Tun <yon.fu111@gmail.com>
2024-12-02 12:47:18 -0500

    featured puppy created

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppylist:", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   
+  const featuredPup = puppies.find((puppy)=> puppy.id===featPupId);
+  console.log(featuredPup);
 
   return (
     <div>
+      {featPupId && <div>
+                      <h2>{featuredPup.name}</h2>
+                      <ul>
+                        <li>Age: {featuredPup.age}</li>
+                        <li>Email: {featuredPup.email}</li>
+                      </ul>
+                    </div>}
       {puppies.map((puppy)=>{
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return <p key={puppy.id} onClick={()=>{setFeatPupId(puppy.id)}}>
+                {puppy.name}
+               </p>;
       })}
     </div>
   );
